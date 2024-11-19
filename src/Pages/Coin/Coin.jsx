b0e8cbd0cc00071217220bdfd6b0269baacc5159
fandi a <fandiiardy@gmail.com>
2024-11-19 12:04:00 +0700

    Add Details Page

--- src/Pages/Coin/Coin.jsx
+++ src/Pages/Coin/Coin.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Coin.css'
+import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../Context/CoinContext';
 
 const Coin = () => {
+
+    const {coinId} = useParams();
+    const [coinData, setCoinData] = useState();
+    const {currency} = useContext(CoinContext);
+
+    const fetchCoinData = async () => {
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-fmdCab9QzQ22woQp4TJUBURC'}
+            };
+
+        fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+            .then(res => res.json())
+            .then(res => setCoinData(res))
+            .catch(err => console.error(err));
+    }
+
+    useEffect(() => {
+        fetchCoinData();
+    },[currency])
+
     return (
-        <div>
-            
+        <div className='coin'>
+            <div className="coin-name">
+                <img src={coinData.image.large} alt="" />
+                <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+            </div>
         </div>
     )
 }
